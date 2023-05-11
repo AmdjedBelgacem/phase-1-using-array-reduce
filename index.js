f127871b4b16cdcbd45af552a7e1d8a43bb44b01
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-05-11 22:49:55 +0300

    Done

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const summer = (acc, val) => {
+    return acc + val;
+  };
+const totalBatteries = batteryBatches.reduce(summer, 0);
+console.log(totalBatteries);
